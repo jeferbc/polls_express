86aa15a3abfcb7250efc55fbb0a4a5a536f6d383
jeffer <jeffe.bernal@gmail.com>
2018-10-02 13:12:26 -0500

    share buttons

--- public/javascripts/share.js
+++ public/javascripts/share.js
@@ -2,3 +2,11 @@ $('#share-link').on('click', function(){
   $('#share-link-modal').modal('toggle');
   console.log('show link');
 });
+
+$('#share-facebook').on('click', function(){
+  const link = $('#share-facebook').data('link')
+  FB.ui({
+    method: 'share',
+    href: link,
+  }, function(response){});
+})
